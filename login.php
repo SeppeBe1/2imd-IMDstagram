f13d3da2b8f045618a9944a364b15627b8a74c8b
Laura Verheyden <lauraverheyden98@hotmail.com>
2021-03-16 18:07:32 +0100

    Bootstrap + login form

--- login.php
+++ login.php
@@ -10,3 +10,49 @@
     var_dump($users);
 
 ?> 
+<!doctype html>
+<html lang="en">
+  <head>
+  
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <title>Login</title>
+  </head>
+  <body>
+    <div class="container-fluid ">
+      <div class="row ">
+        <div class="col-sm ">
+          <div class="formLayout">
+            <form  action="" method="post">
+              <h1 class="form-title">Log in</h1>
+              
+              <div class="form-group">
+                  <label for="username">Username</label>
+                  <input type="text" id="username" name="username">
+              </div>
+
+              <div class="form-group">
+                  <label for="password">Password</label>
+                  <input type="password" id="password" name="password">
+              </div>
+
+              <div class="form-group">
+                  <input type="submit" value="Log In" class="btn btn-primary">
+              </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+    
+
+
+    
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+</html>
