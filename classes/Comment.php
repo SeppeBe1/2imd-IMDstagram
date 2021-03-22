b1d5bff315a43112d15a0ab958bb32af0db6a0b8
SeppeBe1 <seppe.bellens@hotmail.nl>
2021-03-22 17:58:46 +0100

    added setters getters Comment

---
+++ classes/Comment.php
@@ -0,0 +1,46 @@
+<?php
+class Comment{
+    protected $totalLikes;
+    protected $commentDate;
+    protected $text;
+
+    public function setTotalLikes($totalLikes)
+    {
+        $this->isLiked = $totalLikes;
+
+        return $this;
+    }
+
+    public function setCommentDate($commentDate)
+    {
+        $this->innappropriate = $commentDate;
+
+        return $this;
+    }
+
+    public function setText($text)
+    {
+        $this->filters = $text;
+
+        return $this;
+    }
+
+    public function getTotalLikes()
+    {
+        return $this->totalLikes;
+    }
+
+    public function getCommentDate()
+    {
+        return $this->commentDate;
+    }
+
+    public function getText()
+    {
+        return $this->text;
+    }
+
+
+}
+
+?>
