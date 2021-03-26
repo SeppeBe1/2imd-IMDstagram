ddabac4f51faea591f727da529d206375ec09f10
frau96 <frauke_heedfeld@hotmail.com>
2021-03-26 08:50:17 +0100

    Add post.php content

--- post.php
+++ post.php
@@ -1,5 +1,88 @@
 <?php
 
-    echo "Instagram feed posts";
+    // echo "Instagram feed posts";
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+    <link href="css/style.css" rel="stylesheet">    
+    <script src="https://kit.fontawesome.com/a7dc01cef9.js" crossorigin="anonymous"></script>
+
+    <title>Instagram - feed</title>
+</head>
+<body>
+
+
+
+    <div class="container-fluid">
+        
+            <!-- <nav class="navigation-feed"> -->
+                <div class="row navigation-feed">
+                    <div class="col-6">
+                        <a href="#" class="logo-insta">
+                            <img src="./img/instagram-logo.png">
+                        </a>
+                    </div>
+                    <div class="col-6 text-right">
+                        <a href="#">
+                            <span style="font-size: 1.5em; color: Black;">
+                                <i class="fas fa-home nav-icons"></i>
+                            </span>
+                            
+                        </a>
+                        <a href="#">
+                            <span style="font-size: 1.5em; color: Black;">
+                                <i class="far fa-comment-alt nav-icons"></i>
+                            </span>
+                        </a>
+                        <a href="#">
+                            <span style="font-size: 1.5em; color: Black;">
+                                <i class="far fa-compass"></i>
+                            </span>
+                        </a>
+                        <a href="#">
+                            <span style="font-size: 1.5em; color: Black;">
+                                <i class="far fa-heart"></i>
+                            </span>
+                        </a>
+                        <a href="#">
+                            <img src="../2imd-IMDstagram/img/profile-pic.png" class="profile">
+                        </a>
+                    </div>
+                </div>
+            <!-- </nav> -->
+
+            <div class="row">
+                <a href="#">
+                    <img src="../2imd-IMDstagram/img/profile-pic.png" class="stories-img">
+                </a>
+            </div>
+
+                <div class="post-square"></div>
+        
+    </div>
+
+    <div class="container">
+        <h1>Hello World!</h1>
+            <div class="row">
+                <div class="col-sm-3" style="background-color:yellow;">
+                    <p>Lorem ipsum...</p>
+                </div>
+                <div class="col-sm-9" style="background-color:pink;">
+                    <p>Sed ut perspiciatis...</p>
+                </div>
+            </div>
+    </div>
+    
+
+    
+</body>
+</html>
