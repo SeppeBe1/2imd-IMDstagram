248409d365eef932d4283d8bacd960923d2a3586
frau96 <frauke_heedfeld@hotmail.com>
2021-03-28 14:35:45 +0200

    Add feed.php + style file

--- feed.php
+++ feed.php
@@ -1,5 +1,38 @@
 <?php
 
-    echo "Hello World";
+    // echo "Hello World";
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="css/style-feed.css">     
+    <script src="https://kit.fontawesome.com/a7dc01cef9.js" crossorigin="anonymous"></script>
+    <title>Plantstagram</title>
+</head>
+<body>
+
+
+    <header>
+        <div class="container-fluid feed-header">
+            <div class="row justify-content-center">
+                <img src="img/plantstagram-logo.png" class="plant-logo justify-content-center">
+            </div>
+        </div>
+
+    </header>
+
+    <div class="container-fluid">
+
+    </div>
+    
+</body>
+</html>
