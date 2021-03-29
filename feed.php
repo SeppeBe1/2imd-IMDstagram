3f9926fc59ff9af63fe6324b203e0409f685884e
frau96 <frauke_heedfeld@hotmail.com>
2021-03-29 10:42:37 +0200

    Add dummy content to feed

--- feed.php
+++ feed.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="css/style-feed.css">     
     <script src="https://kit.fontawesome.com/a7dc01cef9.js" crossorigin="anonymous"></script>
-    <title>Plantstagram</title>
+    <title>Plantstagram - feed</title>
 </head>
 <body>
 
@@ -28,11 +28,159 @@
             </div>
         </div>
 
+        <div class="container-fluid navigation">
+            <div class="row">
+                <div class="col-7">
+                    <a href="#">
+                        <img src="./img/profile-pic.png" class="profile-pic">
+                    </a>
+                    <a href="#">
+                        <img src="./img/icons/home.svg" class="icon-nav">
+                    </a>
+                    <a href="#">
+                        <img src="./img/icons/+.svg" class="icon-nav">
+                    </a>
+                </div>
+                <div class="col-5">
+                    
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control search-input" placeholder="Search" aria-label="Search" aria-describedby="button-addon2">
+                        <!-- <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button> -->
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
     </header>
 
-    <div class="container-fluid">
+    <main>
 
-    </div>
+    <!-- HIER LOOPEN DOOR VERSCHILLENDE POSTS -->
+            <div class="container post-post">
+                    <div class="row row-first">
+                        <div class="col-2">
+                            <a href="#">
+                                <img src="./img/image-12.png" class="profile-pic-feed">
+                            </a>
+                        </div>
+                        <div class="col-6">
+                            <a href="#"><span class="profile-name">James Ensor</span></a><br>
+                            <a href="#" class="profile-location">Cacun, Mexico</a>
+                        </div>
+                        <div class="col-2">
+                            <a href="#" class="unfollow-button">Unfollow</a>
+                        </div>
+                    </div>
+
+                    <div class="row row-second">
+                        <div class="col-12">
+                            <img src="./img/unsplash1.jpg" class="picture-feed">
+                        </div>
+                        
+                    </div>
+
+                    <div class="row row-third">
+                        <div class="col-1">
+                            <a href="#"><img src="./img/icons/heart.svg" class="icon-feed"></a>
+                        </div>
+                        <div class="col-1">
+                            <p class="number-feed">91</p>
+                        </div>
+                        <div class="col-1">
+                            <a href="#"><img src="./img/icons/chat.svg" class="icon-feed"></a>
+                        </div>
+                        <div class="col-1">
+                            <p class="number-feed">1</p>
+                        </div>
+
+                    </div>
+
+                    <div class="row row-fourth">
+                        <div class="col-12">
+                            <p><span class="profile-name">James Ensor</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis justo sit amet augue tristique viverra quis sit amet tortor. Nulla justo elit, dictum vitae magna consectetur, ultricies ultricies velit. Pellentesque leo odio, egestas sit amet elementum et, consectetur ut ipsum. #kitchen #home #lovemyplants</p>
+                        </div>
+                    </div>
+
+                    <div class="row row-fifth">
+                        <div class="col-12">
+                            <p class="reaction"><span class="profile-name">Paul Delvaux </span>Proin ullamcorper feugiat eros, sit amet accumsan tellus cursus vitae. Etiam feugiat tristique ante, luctus feugiat risus ultrices ut!</p>
+                        </div>  
+                    </div>
+
+                    <div class="row row-sixth">
+                        <div class="col-12">
+                            <p class="timing-feed">14 minutes ago</p>
+                        </div>
+                    </div>
+
+
+            </div>
+
+
+            <!-- HIER IS EEN TWEEDE SOORT VERISE VAN EEN POST-->
+
+            <div class="container post-post">
+                    <div class="row row-first">
+                        <div class="col-2">
+                            <a href="#">
+                                <img src="./img/image-7.png" class="profile-pic-feed">
+                            </a>
+                        </div>
+                        <div class="col-6">
+                            <a href="#"><span class="profile-name">Claude Monet</span></a><br>
+                            <a href="#" class="profile-location">Lyon, France</a>
+                        </div>
+                        <div class="col-2">
+                            <a href="#" class="follow-button">Follow</a>
+                        </div>
+                    </div>
+
+                    <div class="row row-second">
+                        <div class="col-12">
+                            <img src="./img/unsplash3.jpg" class="picture-feed">
+                        </div>
+                        
+                    </div>
+
+                    <div class="row row-third">
+                        <div class="col-1">
+                            <a href="#"><img src="./img/icons/heart-outlines.svg" class="icon-feed"></a>
+                        </div>
+                        <div class="col-1">
+                            <p class="number-feed">13</p>
+                        </div>
+                        <div class="col-1">
+                            <a href="#"><img src="./img/icons/chat.svg" class="icon-feed"></a>
+                        </div>
+                        <div class="col-1">
+                            <p class="number-feed">1</p>
+                        </div>
+
+                    </div>
+
+                    <div class="row row-fourth">
+                        <div class="col-12">
+                            <p><span class="profile-name">Claude Monet</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis justo sit amet augue tristique viverra quis sit amet tortor. Nulla justo elit, dictum vitae magna consectetur, ultricies ultricies velit. Pellentesque leo odio, egestas sit amet elementum et, consectetur ut ipsum. #kitchen #home #lovemyplants</p>
+                        </div>
+                    </div>
+
+                    <div class="row row-fifth">
+                        <div class="col-12">
+                            <p class="reaction"><span class="profile-name">Eduard Manet </span>Proin ullamcorper feugiat eros, sit amet accumsan tellus cursus vitae. Etiam feugiat tristique ante, luctus feugiat risus ultrices ut!</p>
+                        </div>  
+                    </div>
+
+                    <div class="row row-sixth">
+                        <div class="col-12">
+                            <p class="timing-feed">14 minutes ago</p>
+                        </div>
+                    </div>
+
+
+            </div>
+
+    </main>
     
 </body>
 </html>
