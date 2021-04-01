4dba8c4b2b14708a0a9b887a1d004b2577569b4e
Laura Verheyden <lauraverheyden98@hotmail.com>
2021-04-02 00:21:20 +0200

    add gallery to profile page + css

--- profile.php
+++ profile.php
@@ -102,3 +102,54 @@
                 </div>
             </div>     
         </div>
+
+        <!-- LOOPEN OVER POSTS !-->
+
+        <div class="container-fluid container-gallery">
+            <div class="row">
+                <div class="col-4">
+                    <img class="img-thumbnail"src="https://images.pexels.com/photos/583791/pexels-photo-583791.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" >
+                </div>
+
+                <div class="col-4 ">
+                    <img class="img-thumbnail" src="https://images.pexels.com/photos/3673763/pexels-photo-3673763.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" class="img-responsive">
+                </div>
+
+                <div class="col-4 ">
+                    <img class="img-thumbnail" src="https://images.pexels.com/photos/4019666/pexels-photo-4019666.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" class="img-responsive">
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-4">
+                    <img class="img-thumbnail "src="https://images.pexels.com/photos/3255246/pexels-photo-3255246.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" >
+                </div>
+
+                <div class="col-4 ">
+                    <img class="img-thumbnail" src="https://images.pexels.com/photos/3817847/pexels-photo-3817847.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="img-responsive">
+                </div>
+
+                <div class="col-4 ">
+                    <img class="img-thumbnail" src="https://images.pexels.com/photos/355279/pexels-photo-355279.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="img-responsive">
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-4 square">
+                    <img class="img-thumbnail"src="https://images.pexels.com/photos/771319/pexels-photo-771319.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" >
+                </div>
+
+                <div class="col-4 ">
+                    <img class="img-thumbnail" src="https://images.pexels.com/photos/4894423/pexels-photo-4894423.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="img-responsive">
+                </div>
+
+                <div class="col-4 ">
+                    <img class="img-thumbnail" src="https://images.pexels.com/photos/5720091/pexels-photo-5720091.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="img-responsive">
+                </div>
+            </div>
+            
+
+    </main>
+
+</body>
+</html>
