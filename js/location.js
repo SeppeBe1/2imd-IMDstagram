e97b1b5b77530334e44229ae1a7f2121fd194cc7
SCRUFFYXNERFHERDER <marina.kan@hotmail.com>
2021-04-30 10:12:59 +0200

    geolocation 'city/town'

--- js/location.js
+++ js/location.js
@@ -9,9 +9,10 @@ function getLocation() {
 }
 
 function showPosition(position) {
-        fetch('https://eu1.locationiq.com/v1/reverse.php?key=pk.2aaa45b75aab4c4d8a85def3455e7590&lat=' + position.coords.latitude + '&' + 'lon=' + position.coords.longitude  + '&format=json')
+    fetch('https://eu1.locationiq.com/v1/reverse.php?key=pk.2aaa45b75aab4c4d8a85def3455e7590&lat=' + position.coords.latitude + '&' + 'lon=' + position.coords.longitude + '&format=json')
         .then(response => response.json())
-        .then(function(data){
-            document.getElementById('location-search').value = data.address.town + ', ' + data.address.country;
+        .then(function(data) {
+            //console.log(data);
+            document.getElementById('location-search').value = (data.address.town || data.address.city) + ', ' + data.address.country;
         });
 }
